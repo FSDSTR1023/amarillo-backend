f03ce81c0d1eb75376d8cca613b464cc057447e9
tonebonedev <tonifcb@gmail.com>
2023-12-05 17:47:50 +0200

    backend created, connection to mongoDB done, readme file created

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # amarillo-backend
+
+# To start
+
+1. yarn
+2. yarn start
+3. yarn mongo
+4. yarn add mongoose
+
+# Prepare .env
+
+5. create .env file
+6. yarn add dotenv
+7. nodemon app.js
+
+username: tonifcb
+pw: GeNL6wlPZApzpFC0
---
+++ app.js
@@ -0,0 +1,67 @@
+import createError from "http-errors";
+import express, { json, urlencoded } from "express";
+import { join } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index.route.js";
+import usersRouter from "./routes/users.route.js";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoDB =
+  "mongodb+srv://" +
+  process.env.DB_USER +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@" +
+  process.env.DB_SERVER +
+  "/" +
+  process.env.DB_NAME +
+  "?retryWrites=true&w=majority";
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  console.log(process.env.DB_USER);
+  res.send("hello toni");
+});
+
+// view engine setup
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
